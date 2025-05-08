b8c52dda985602c67c0e7e5a817103aad77df462
野迂迂 <yangwei1@outlook.com>
2025-05-08 16:27:48 +0800

    feat: improve UX for log usage (#24)

--- packages/core/src/composables/useOutputChannel.ts
+++ packages/core/src/composables/useOutputChannel.ts
@@ -1,10 +1,12 @@
-import { window } from 'vscode'
+import { type LogOutputChannel, type OutputChannel, window } from 'vscode'
 import { useDisposable } from './useDisposable'
 
 /**
  * @reactive `window.createOutputChannel`
  * @category view
  */
-export function useOutputChannel(name: string, languageId?: string) {
-  return useDisposable(window.createOutputChannel(name, languageId))
+export function useOutputChannel(name: string, options: { log: true }): LogOutputChannel
+export function useOutputChannel(name: string, languageId?: string): OutputChannel
+export function useOutputChannel(name: string, languageIdOrOptions?: string | { log: true }) {
+  return useDisposable(window.createOutputChannel(name, <string>languageIdOrOptions))
 }
